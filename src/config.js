56cf510cb0919c59193b5fe1132a532d1a6f7dfe
abinashbhattarai <bhattarai.abinash@gmail.com>
2019-03-12 13:49:43 -0500

    .gitignore updated

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const fireBaseConfig = {
-    apiKey: "AIzaSyAPw6r7lXgnVCizZIiXPMKoGdPYlJXO6yM",
-    authDomain: "reactpanel-a934f.firebaseapp.com",
-    databaseURL: "https://reactpanel-a934f.firebaseio.com",
-    projectId: "reactpanel-a934f",
-    storageBucket: "reactpanel-a934f.appspot.com",
-    messagingSenderId: "808199694556"
-};
-
